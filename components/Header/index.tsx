c2b43f534a3b113ed141ffc66c86bec5da684296
Sebastian Alfieri <sebasalfieri@gmail.com>
2023-06-02 16:01:11 -0300

    clean

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,42 +1,28 @@
-import React, { FC } from 'react';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
-import cs from 'classnames';
-import { ReactSVG } from 'react-svg';
-import s from './Header.module.css';
+import React, { FC } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import s from "./Header.module.css";
 
 interface HeaderProps {
   login?: boolean;
 }
 
-const Header: FC<HeaderProps> = ({ login }) => {
-  const header = cs(s.header, {
-    [s.header__login]: login == true
-  });
-
-  const logo = cs(s.mastercard, {
-    [s.loginMastercard]: login == true
-  });
-
+const Header = () => {
   return (
     <motion.header
-      className={header}
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1 } }}>
+      animate={{ opacity: 1, transition: { duration: 1 } }}
+    >
       <div className={s.header__spotify}>
-        <Image src="/images/logo-spotify.png" alt="bySpotify" width={422} height={63} />
-      </div>
-
-      <div className={s.header__mastercard}>
-        <ReactSVG
-          className={logo}
-          src="/icons/logo.svg"
-          wrapper="span"
-          beforeInjection={(svg: any) => {
-            svg.classList.add('svg__icon');
-          }}
+        <Image
+          src="/images/logo-spotify.png"
+          alt="bySpotify"
+          width={422}
+          height={63}
         />
       </div>
+
+      <div className={s.header__mastercard}></div>
     </motion.header>
   );
 };
